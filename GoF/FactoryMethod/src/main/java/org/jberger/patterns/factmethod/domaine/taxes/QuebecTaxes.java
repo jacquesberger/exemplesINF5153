9eebe728a6fe02f17e3b94a32af1e79cac05ccff
Jacques Berger <jacques.berger@code3.ca>
2015-10-20 00:36:12 -0400

    Exemple du patron Factory Method

---
+++ GoF/FactoryMethod/src/main/java/org/jberger/patterns/factmethod/domaine/taxes/QuebecTaxes.java
@@ -0,0 +1,34 @@
+/* Copyright 2015 Jacques Berger
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package org.jberger.patterns.factmethod.domaine.taxes;
+
+import org.jberger.patters.factmethod.domaine.money.Currency;
+
+class QuebecTaxes implements TaxCalculator {
+    private static final double CANADA_RATE = 0.05;
+    private static final double QUEBEC_RATE = 0.09975;
+    
+    public QuebecTaxes() {
+    }
+    
+    @Override
+    public TaxTotals calculate(Currency subtotal) {
+        Currency federal = subtotal.applyPourcentage(CANADA_RATE);
+        Currency provincial = subtotal.applyPourcentage(QUEBEC_RATE);
+        Currency total = subtotal.add(federal.add(provincial));
+        return new TaxTotals(subtotal, federal, provincial, total);
+    }
+}
